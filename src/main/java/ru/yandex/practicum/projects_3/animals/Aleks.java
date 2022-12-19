afc9a9851b62ebcc69bcbe79b81732cd1b4b9b89
subbotin-n-o <subbotin.n.o@gmail.com>
2022-12-19 22:24:20 +0400

    #6 fix Lion.java, added Aleks.java

---
+++ src/main/java/ru/yandex/practicum/projects_3/animals/Aleks.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.projects_3.animals;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Aleks extends Lion {
+
+    private final List<String> friendList = Arrays.asList("Марти", "Глория", "Мелман");
+    private final String placeOfResidence = "Нью-Йоркский зоопарк";
+
+    public Aleks(Feline feline) throws Exception {
+        super(feline, "Самец");
+    }
+
+    @Override
+    public int getKittens() {
+        return predator.getKittens(0);
+    }
+
+    public List<String> getFriends() {
+        return friendList;
+    }
+
+    public String getPlaceOfLiving() {
+        return placeOfResidence;
+    }
+}
--- src/main/java/ru/yandex/practicum/projects_3/animals/Lion.java
+++ src/main/java/ru/yandex/practicum/projects_3/animals/Lion.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 public class Lion {
 
-    boolean hasMane;
+    protected final boolean hasMane;
+    protected final Feline predator;
 
-    public Lion(String sex) throws Exception {
+    public Lion(Feline feline, String sex) throws Exception {
+        this.predator = feline;
+
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
@@ -16,10 +19,8 @@ public class Lion {
         }
     }
 
-    Feline feline = new Feline();
-
     public int getKittens() {
-        return feline.getKittens();
+        return predator.getKittens();
     }
 
     public boolean doesHaveMane() {
@@ -27,6 +28,6 @@ public class Lion {
     }
 
     public List<String> getFood() throws Exception {
-        return feline.getFood("Хищник");
+        return predator.getFood("Хищник");
     }
 }
